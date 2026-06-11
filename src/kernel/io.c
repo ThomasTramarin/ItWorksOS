2ed034cc6190b05903947503befb41da2a024504
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-06-11 13:30:24 +0200

    feat: add io_wait function

--- src/kernel/io.c
+++ src/kernel/io.c
@@ -1,5 +1,7 @@
 #include "io.h"
 
+#define UNUSED_PORT 0x80
+
 unsigned char inb(unsigned short port) {
   unsigned char res;
   asm volatile("inb %1, %0" : "=a"(res) : "Nd"(port));
@@ -9,3 +11,5 @@ unsigned char inb(unsigned short port) {
 void outb(unsigned short port, unsigned char value) {
   asm volatile("outb %0, %1" : : "a"(value), "Nd"(port));
 }
+
+void io_wait(void) { outb(UNUSED_PORT, 0); }
--- src/kernel/io.h
+++ src/kernel/io.h
@@ -4,4 +4,6 @@
 void outb(unsigned short port, unsigned char value);
 unsigned char inb(unsigned short port);
 
+void io_wait(void);
+
 #endif
