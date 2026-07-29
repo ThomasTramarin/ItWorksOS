0c5d85c501c1572bfaa25b33daa5cd0a5d05b3eb
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-07-29 21:17:34 +0200

    refactor: use printk logging in kmain

--- src/kernel/core/kmain.c
+++ src/kernel/core/kmain.c
@@ -1,49 +1,53 @@
-#include "boot/boot_info.h"
 #include <arch/x86/interrupts/exceptions.h>
 #include <arch/x86/interrupts/idt.h>
 #include <arch/x86/interrupts/pic.h>
 #include <arch/x86/timer/pit.h>
+#include <boot/boot_info.h>
 #include <core/kmain.h>
 #include <drivers/video/vga/vga.h>
+#include <kernel/panic.h>
+#include <kernel/printk.h>
+#include <kernel/syslog.h>
 #include <klib/cui.h>
 
 void kmain(uint32_t magic, boot_info_t *info) {
+  syslog_init();
+
   cui_init(VGA_COLOR_WHITE, VGA_COLOR_BLUE, 0);
 
   if (magic != BOOT_MAGIC) {
-    cui_printf("Error: invalid magic. System Halted\n");
-    goto halt;
+    panic("Invalid boot magic value");
   }
 
-  cui_klog("Memory Map:\n");
+  pr_info("Memory Map:\n");
   for (uint32_t i = 0; i < info->memory_map_count; i++) {
     boot_mem_map_entry_t entry = info->memory_map_ptr[i];
 
     uint32_t base_low = (uint32_t)entry.base;
     uint32_t len_low = (uint32_t)entry.length;
 
-    cui_klog("\t%d. Base: 0x%x, Length: 0x%x, Type: %x, Attr: %x\n", i,
-             base_low, len_low, entry.type, entry.attr);
+    pr_info("%d. Base: 0x%x, Length: 0x%x, Type: %x, Attr: %x\n", i, base_low,
+            len_low, entry.type, entry.attr);
   }
 
   idt_init();
-  cui_klog("IDT and ISR configurated.\n");
+  pr_info("IDT and ISR configurated.\n");
 
   exceptions_init();
-  cui_klog("Exception handling enabled.\n");
+  pr_info("Exception handler enabled.\n");
 
   pic_init();
-  cui_klog("PIC configurated.\n");
+  pr_info("PIC configurated.\n");
 
   timer_init();
-  cui_klog("PIT configurated.\n");
+  pr_info("PIT configurated.\n");
 
   asm volatile("sti");
-  cui_klog("Interrupts enabled.\n");
+  pr_info("Interrupts enabled.\n");
 
-  cui_puts("\nWelcome to ItWorksOnMyHP\n");
+  cui_puts("Welcome to ItWorksOnMyHP");
 
-halt:
+  // CPU halt
   while (1) {
     asm volatile("hlt");
   }
