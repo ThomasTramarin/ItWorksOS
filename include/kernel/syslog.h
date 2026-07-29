a96d2ed554a2e4ae6a86b591954fc534b2a50a1c
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-07-29 15:59:56 +0200

    feat(log): implement syslog in-memory ring buffer

---
+++ include/kernel/syslog.h
@@ -0,0 +1,48 @@
+#ifndef KERNEL_SYSLOG_H
+#define KERNEL_SYSLOG_H
+
+#include <base/stdbool.h>
+#include <base/stddef.h>
+
+#define SYSLOG_BUFFER_SIZE 4096
+
+/**
+ * @file syslog.h
+ * @brief Kernel in-memory log buffer interface.
+ */
+
+/**
+ * @brief Initializes the kernel syslog ring buffer.
+ * Must be called during early boot before any logs are generated.
+ */
+void syslog_init(void);
+
+/**
+ * @brief Appends a single character to the kernel log buffer.
+ * Overwrites the oldest log entry if the buffer is full.
+ */
+void syslog_putc(char c);
+
+/**
+ * @brief Appends a null-terminated string to the kernel log buffer.
+ */
+void syslog_write(const char *str);
+
+/**
+ * @brief Gets the current number of bytes stored in the log buffer.
+ *
+ * @return Number of stored bytes.
+ */
+size_t syslog_len(void);
+
+/**
+ * @brief Reads logs from the buffer into a destination array without consuming
+ * them.
+ *
+ * @param dst pointer to the output buffer where logs will be copied.
+ * @param size maximum number of bytes to copy.
+ * @return number of bytes actually copied into dst.
+ */
+size_t syslog_read(char *dst, size_t size);
+
+#endif
---
+++ src/kernel/core/syslog.c
@@ -0,0 +1,62 @@
+#include <kernel/syslog.h>
+#include <klib/memory.h>
+#include <klib/ringbuf.h>
+
+// Logs Ring Buffer
+static uint8_t syslog_storage[SYSLOG_BUFFER_SIZE];
+static ringbuf_t syslog_rb;
+static bool syslog_ready = false;
+
+void syslog_init(void) {
+  ringbuf_init(&syslog_rb, syslog_storage, SYSLOG_BUFFER_SIZE);
+  syslog_ready = true;
+}
+
+void syslog_putc(char c) {
+  if (!syslog_ready)
+    return;
+  ringbuf_overwrite(&syslog_rb, (uint8_t)c);
+}
+
+void syslog_write(const char *str) {
+  while (*str) {
+    syslog_putc(*str);
+    str++;
+  }
+}
+
+size_t syslog_len(void) {
+  if (!syslog_ready)
+    return 0;
+
+  return ringbuf_len(&syslog_rb);
+}
+
+size_t syslog_read(char *dst, size_t size) {
+  // tail represents the reader starting position
+  // tail is not incremented so this function does not consume bytes
+  // tail is only incremented by ringbuf_overwrite when the buffer becomes full
+
+  if (!syslog_ready)
+    return 0;
+
+  size_t len = ringbuf_len(&syslog_rb);
+
+  // leave space for '\0'
+  if (len >= size) {
+    len = size - 1;
+  }
+
+  // copy len bytes, starting from tail, without consume them
+  for (size_t i = 0; i < len; i++) {
+    uint8_t c;
+
+    if (ringbuf_peek(&syslog_rb, i, &c)) {
+      dst[i] = c;
+    }
+  }
+
+  dst[len] = '\0';
+
+  return len;
+}
