76966dbc99796dde30f954ace8c3456c7b322505
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-08-01 19:55:18 +0200

    feat(x86): implement port I/O HAL

---
+++ src/arch/x86/hal/port.c
@@ -0,0 +1,16 @@
+#include <arch/x86/io.h>
+#include <hal/port.h>
+
+/**
+ * @file port.c
+ *
+ * @brief x86 Port I/O HAL implementation
+ */
+
+uint8_t hal_port_read8(uint16_t port) { return x86_inb(port); }
+uint16_t hal_port_read16(uint16_t port) { return x86_inw(port); }
+uint32_t hal_port_read32(uint16_t port) { return x86_inl(port); }
+
+void hal_port_write8(uint16_t port, uint8_t value) { x86_outb(port, value); }
+void hal_port_write16(uint16_t port, uint16_t value) { x86_outw(port, value); }
+void hal_port_write32(uint16_t port, uint32_t value) { x86_outl(port, value); }
