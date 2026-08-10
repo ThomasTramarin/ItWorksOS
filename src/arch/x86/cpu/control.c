814294ce71848a77ee3bc1a3d71b64d3f99ac900
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-08-10 16:49:52 +0200

    feat(x86): add CR0 control register access helpers

--- src/arch/x86/cpu/control.c
+++ src/arch/x86/cpu/control.c
@@ -9,3 +9,13 @@ uint32_t x86_cr3_read(void) {
   __asm__ __volatile__("mov %%cr3, %0" : "=r"(value));
   return value;
 }
+
+void x86_cr0_write(uint32_t value) {
+  __asm__ __volatile__("mov %0, %%cr0" ::"r"(value));
+}
+
+uint32_t x86_cr0_read(void) {
+  uint32_t value;
+  __asm__ __volatile__("mov %%cr0, %0" : "=r"(value));
+  return value;
+}
--- src/arch/x86/include/arch/x86/cpu/control.h
+++ src/arch/x86/include/arch/x86/cpu/control.h
@@ -3,6 +3,24 @@
 #include <base/stdint.h>
 
 /*
+ * CR0
+ */
+#define X86_CR0_PE BIT(0)  /* Protection Enable */
+#define X86_CR0_MP BIT(1)  /* Monitor Coprocessor */
+#define X86_CR0_EM BIT(2)  /* Emulation */
+#define X86_CR0_TS BIT(3)  /* Task Switched */
+#define X86_CR0_ET BIT(4)  /* Extension Type */
+#define X86_CR0_NE BIT(5)  /* Numeric Error */
+#define X86_CR0_WP BIT(16) /* Write Protect */
+#define X86_CR0_AM BIT(18) /* Alignment Mask */
+#define X86_CR0_NW BIT(29) /* Not Write-through */
+#define X86_CR0_CD BIT(30) /* Cache Disable */
+#define X86_CR0_PG BIT(31) /* Paging */
+
+void x86_cr0_write(uint32_t value);
+uint32_t x86_cr0_read(void);
+
+/*
  * The CR3 register contains the physical address of the
  * Page Directory currently used by the CPU.
  */
