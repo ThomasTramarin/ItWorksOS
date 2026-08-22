f409634253f42a74b87b51571d913919ab3328d7
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-08-22 18:57:48 +0200

    feat(mm): add memory management subsystem initialization

---
+++ include/mm/mm.h
@@ -0,0 +1,21 @@
+#ifndef MM_MM_H
+#define MM_MM_H
+
+/* Include memory subsystems */
+#include <mm/kheap.h>
+#include <mm/pmm.h>
+#include <mm/vmm.h>
+
+/**
+ * @brief Initialize the entire memory management subsystem
+ *
+ * Initialize in sequence:
+ *  1. Physical Memory Manager
+ *  2. Virtual Memory Manager
+ *  3. Kernel Heap
+ *
+ * @return KERR_OK or negative kernel error code
+ */
+int32_t mm_init(const struct boot_mem_map_entry *map, uint16_t count);
+
+#endif
---
+++ src/mm/mm.c
@@ -0,0 +1,12 @@
+#include <kernel/error.h>
+#include <mm/mm.h>
+
+int32_t mm_init(const struct boot_mem_map_entry *map, uint16_t count) {
+  KERR_TRY(pmm_init(map, count));
+
+  KERR_TRY(vmm_init());
+
+  KERR_TRY(kheap_init());
+
+  return KERR_OK;
+}
