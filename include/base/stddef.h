9f542ec63cdec185cada0fe4eb62f2b58ac4b4b4
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-08-22 18:40:47 +0200

    feat(base): add container_of macro

--- include/base/stddef.h
+++ include/base/stddef.h
@@ -10,4 +10,7 @@ typedef uint32_t size_t;
 
 #define offsetof(type, member) ((size_t)&(((type *)0)->member))
 
+#define container_of(ptr, type, member)                                        \
+  ((type *)((char *)(ptr) - offsetof(type, member)))
+
 #endif
