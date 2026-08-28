b5f30f66d8f8690a80d9045644750e9703450ed1
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-08-28 19:18:49 +0200

    feat(klib): add bitmap size macros

--- kernel/include/klib/bitmap.h
+++ kernel/include/klib/bitmap.h
@@ -10,6 +10,13 @@ struct bitmap {
   size_t nelems;
 };
 
+#define BITMAP_BITS_PER_ELEM 32
+
+#define BITMAP_ELEMS(nbits)                                                    \
+  (((nbits) + BITMAP_BITS_PER_ELEM - 1) / BITMAP_BITS_PER_ELEM)
+
+#define BITMAP_SIZE(nbits) ((nbits) * sizeof(uint32_t))
+
 void bitmap_init(struct bitmap *bm, uint32_t *buffer, size_t nbits);
 void bitmap_set(struct bitmap *bm, size_t bitpos);
 void bitmap_clear(struct bitmap *bm, size_t bitpos);
--- kernel/klib/bitmap.c
+++ kernel/klib/bitmap.c
@@ -3,27 +3,33 @@
 #include <base/stdbool.h>
 #include <klib/bitmap.h>
 
-#define BITS_PER_ELEM 32
-
 void bitmap_init(struct bitmap *bm, uint32_t *buffer, size_t nbits) {
   bm->data = buffer;
   bm->nbits = nbits;
 }
 
 void bitmap_set(struct bitmap *bm, size_t bitpos) {
-  bm->data[bitpos / BITS_PER_ELEM] |= BIT(bitpos % BITS_PER_ELEM);
+  bm->data[bitpos / BITMAP_BITS_PER_ELEM] |= BIT(bitpos % BITMAP_BITS_PER_ELEM);
 }
 
 void bitmap_clear(struct bitmap *bm, size_t bitpos) {
-  bm->data[bitpos / BITS_PER_ELEM] &= ~BIT(bitpos % BITS_PER_ELEM);
+  bm->data[bitpos / BITMAP_BITS_PER_ELEM] &=
+      ~BIT(bitpos % BITMAP_BITS_PER_ELEM);
 }
 
 bool bitmap_test(const struct bitmap *bp, size_t bitpos) {
-  return (bp->data[bitpos / BITS_PER_ELEM] & BIT(bitpos % BITS_PER_ELEM)) != 0;
+  return (bp->data[bitpos / BITMAP_BITS_PER_ELEM] &
+          BIT(bitpos % BITMAP_BITS_PER_ELEM)) != 0;
+}
+
+size_t bitmap_size(size_t nbits) {
+  size_t nelems = (nbits + BITMAP_BITS_PER_ELEM - 1) / BITMAP_BITS_PER_ELEM;
+  return nelems * sizeof(uint32_t);
 }
 
 bool bitmap_find_zero(const struct bitmap *bm, size_t *bitpos) {
-  for (size_t i = 0; i < ((bm->nbits + BITS_PER_ELEM - 1) / BITS_PER_ELEM);
+  for (size_t i = 0;
+       i < ((bm->nbits + BITMAP_BITS_PER_ELEM - 1) / BITMAP_BITS_PER_ELEM);
        i++) {
 
     uint32_t free = ~bm->data[i];
@@ -31,10 +37,10 @@ bool bitmap_find_zero(const struct bitmap *bm, size_t *bitpos) {
     if (free == 0)
       continue;
 
-    for (size_t bit = 0; bit < BITS_PER_ELEM; bit++) {
+    for (size_t bit = 0; bit < BITMAP_BITS_PER_ELEM; bit++) {
       if (free & BIT(bit)) {
 
-        size_t pos = i * BITS_PER_ELEM + bit;
+        size_t pos = i * BITMAP_BITS_PER_ELEM + bit;
 
         if (pos >= bm->nbits)
           return false;
