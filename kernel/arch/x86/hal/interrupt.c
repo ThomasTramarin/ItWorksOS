f30aab078e266e2b47e41738cd6b3071467261d9
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-09-03 23:33:27 +0200

    feat(hal,x86): add save and restore interrupt state functions

--- kernel/arch/x86/hal/interrupt.c
+++ kernel/arch/x86/hal/interrupt.c
@@ -7,3 +7,7 @@ void hal_interrupt_enable(void) { x86_sti(); }
 void hal_interrupt_disable(void) { x86_cli(); }
 
 bool hal_interrupt_enabled(void) { return x86_interrupts_enabled(); }
+
+bool hal_interrupt_save_disable(void) { return x86_interrupt_save_disable(); }
+
+void hal_interrupt_restore(bool state) { x86_interrupt_restore(state); }
--- kernel/include/hal/interrupt.h
+++ kernel/include/hal/interrupt.h
@@ -8,4 +8,18 @@ void hal_interrupt_enable(void);
 void hal_interrupt_disable(void);
 bool hal_interrupt_enabled(void);
 
+/**
+ * @brief Disable hardware interrupts and save the previous state
+ *
+ * @return The previous interrupt state (true = enabled, false = disabled)
+ */
+bool hal_interrupt_save_disable(void);
+
+/**
+ * @brief Restore interrupts to the previous state
+ *
+ * @param state The value returned by hal_interrupt_save_disable()
+ */
+void hal_interrupt_restore(bool state);
+
 #endif
