808a5f78529e15d34ad3eb1d425deb44f6fc4751
Thomas Tramarin <thomas.tramarin.dev@gmail.com>
2026-09-19 23:11:55 +0200

    feat(base): add ALIGN_TEST macro

--- kernel/include/base/align.h
+++ kernel/include/base/align.h
@@ -9,4 +9,8 @@
 #define ALIGN_UP(x, align) (((x) + (align) - 1) & ~((align) - 1))
 #define ALIGN_DOWN(x, align) ((x) & ~((align) - 1))
 
+/**
+ * @brief Test if the number x is a multiple of align
+ */
+#define ALIGN_TEST(x, align) ((x) % (align) == 0)
 #endif
